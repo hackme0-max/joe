431fb0df2228efa89367a5839c332ebc9818a662
hackme0-max <snpxblack2006@gmail.com>
2025-10-29 18:54:36 +0200

    Delete telegram-config.js

--- telegram-config.js
+++
@@ -1,7 +0,0 @@
-
-
-const TELEGRAM_CONFIG = {
-    BOT_TOKEN: '8187913757:AAFwQeSZQFyff044B5A7eg2_uwLLGOZqBwA',  // Replace with your bot token from BotFather
-    CHAT_ID: '-1003136241815'       // Replace with your chat ID
-};
-
